8941f5fa3dd5a010ea057efcd8420c2bd0f644ac
mnguyen <mnguyen@eecs.wsu.edu>
2021-04-19 01:40:43 -0700

    Replace User.cs

--- YelpUI/User.cs
+++ YelpUI/User.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace YelpUI
 {
-<<<<<<< HEAD
-    public class User
-=======
     public class User : SQLQueries
->>>>>>> Refactored several elements in the code
     {
         public string userID { get; set; }
         public double avg_stars { get; set; }
@@ -24,13 +20,6 @@ namespace YelpUI
         public int totalLikes { get; set; }
         public double latitude { get; set; }
         public double longitude { get; set; }
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
-
->>>>>>> Refactored several elements in the code
-=======
->>>>>>> Reorganized code
     }
 }
